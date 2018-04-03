721676a8a1f47d43375941d184e475b836672b38
Yadkee <yadkee@gmail.com>
2018-04-03 20:04:10 +0200

    Added buttons

--- main.py
+++ main.py
@@ -26,9 +26,38 @@ CHARACTERS = dict(zip(NAMES, CHARS))
 
 class App(tk.Frame):
     def __init__(self, master, imageSize):
-        tk.Frame.__init__(self, master)
+        imageSize8 = 8 * imageSize + 32
+        tk.Frame.__init__(self, master, width=imageSize8, height=imageSize8)
+        self.grid_propagate(0)
+        self.imageSize = imageSize
         if HAS_PIL:
             self.load_images()
+
+        for i in range(8):
+            self.columnconfigure(i, weight=1, uniform="same")
+            self.rowconfigure(i, weight=1, uniform="same")
+        self.buttons = []
+        for a in range(64):
+            color = "plum" if (a + a // 8) & 1 else "white"
+            button = tk.Button(self, command=self.press(a),
+                               bg=color, activebackground=color,
+                               font=("Arial", -imageSize))
+            button.grid(column=a % 8, row=a // 8, sticky="NSEW")
+            self.buttons.append(button)
+
+    def press(self, button):
+        def wrapper():
+            print(button)
+        return wrapper
+
+    def change_button(self, button, value):
+        d, m = divmod(value, 10)
+        toIndex = m - 1 + d * 6
+        if HAS_PIL:
+            kw = {"image": self.images[toIndex]}
+        else:
+            kw = {"text": CHARS[toIndex]}
+        self.buttons[button].config(**kw)
 
     def load_images(self):
         folder = join("media", "")
@@ -36,7 +65,7 @@ class App(tk.Frame):
                             Image.open(
                                 folder + name + ".png"
                             ).resize(
-                                (imageSize, imageSize)
+                                (self.imageSize, self.imageSize)
                                 ), Image.ANTIALIAS
                         ) for name in NAMES]
 
@@ -45,11 +74,14 @@ if __name__ == "__main__":
     with open("config.json") as f:
         loaded = load(f)
         imageSize = int(loaded["imageSize"])
+    imageSize8 = 8 * imageSize + 32
     root = tk.Tk()
     root.title("Chess")
-    root.config(bg="white")
+    root.config(bg="gray")
     root.iconbitmap(default=join("media", "icon.ico"))
-    root.geometry("300x300+0+0")
+    root.geometry("%dx%d+0+0" % (imageSize8, imageSize8))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
     app = App(root, imageSize)
     app.grid(column=0, row=0)
     root.mainloop()
