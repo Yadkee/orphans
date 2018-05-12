63ca16c254d994949b18fc0db1c8ea2d65b2559c
Yadkee <yadkee@gmail.com>
2018-05-12 18:51:08 +0200

    Swapped to place (grid manager)

--- widgets/idle.py
+++ widgets/idle.py
@@ -6,16 +6,19 @@ class Idle(tk.Frame):
     def __init__(self, master, callback):
         def click():
             callback(self.entry.get())
-        tk.Frame.__init__(self, master, width=800, height=800)
+        tk.Frame.__init__(self, master)
         # Read serverAdress file
         with open("serverAddress", "rb") as f:
             encoded, _ = f.read().split(b"\n\r", 1)
         name = encoded.decode()
         self.label = tk.Label(self, text=name, font=("Times", 50, "bold"))
-        self.entry = tk.Entry(self)
+        self.entry = tk.Entry(self, width=16, font=("Consolas", 20))
         self.entry.insert(0, "MyName")
-        self.button = tk.Button(self, text="JOIN", command=click)
+        self.button = tk.Button(self, text="JOIN", command=click,
+                                font=("Times", 20, "bold"))
 
-        self.label.grid(column=0, row=0, columnspan=3)
-        self.entry.grid(column=1, row=1)
-        self.button.grid(column=1, row=2)
+        self.label.place(relx=.5, rely=.2,
+                         relwidth=.9, anchor="center")
+        self.entry.place(relx=.5, rely=.6,
+                         relwidth=.5, anchor="center")
+        self.button.place(relx=.5, rely=.8, anchor="center")
