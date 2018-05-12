73fb05c71a78d566245ea837e32682a590c83495
Yadkee <yadkee@gmail.com>
2018-05-13 01:51:08 +0200

    Method for closing the hub and updating the user and queue lists

--- widgets/hub.py
+++ widgets/hub.py
@@ -4,8 +4,20 @@ import tkinter as tk
 
 class Hub(tk.Frame):
     def __init__(self, master, client, tagStr):
+        def update():
+            users = client.users
+            queue = client.queue
+            lu, lq = self.last
+            if lu != users:
+                pass  # Update users list
+            if lq != queue:
+                pass  # Update queue list
+            self.last = (users, queue)
         tk.Frame.__init__(self, master)
         master.minsize(600, 400)
+        client.update = update
+        self.client = client
+        self.last = (None, None)
 
         self.serverLabel = tk.Label(self, text=client.server[0],
                                     font=("Times", 50, "bold"))
@@ -22,3 +34,7 @@ class Hub(tk.Frame):
         self.serverLabel.grid(column=0, row=0, columnspan=2, sticky="W")
         self.clientLabel.grid(column=2, row=0, sticky="E")
         self.button.grid(column=1, row=2, sticky="SEW")
+
+    def close(self):
+        self.grid_forget()
+        self.client.update = lambda: None
